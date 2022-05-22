da3dec42b5d9329fa4bf7862461c62bb907e6f64
Terrior <werbird10@gmail.com>
2022-05-22 01:22:01 -0700

    first commit, setting up pyo3

---
+++ py_tester/main.py
@@ -0,0 +1,23 @@
+import maturintest as mt
+
+'''
+print(lt.hellopy())
+
+x = lt.Greeter()
+print(x.greet("Papa"))
+
+y = lt.Storage(3)
+print(y.num)
+y.num = 4
+print(y.num)
+#rint(y.num)
+#print(y.half())
+
+z = lt.Storage(2)
+z.num = 6
+print(y.sum_nums(z))
+
+NN = lt.NeuralNetwork(None)
+'''
+
+print(mt.sum_as_string(3, 4))
